5dd503f1b3415931acf53c9b0669bd332650209f
Michelle282 <michelleadunne@gmail.com>
2021-05-19 19:44:07 +0100

    DA Finance Datacamp Files Mod1

---
+++ m1c1.py
@@ -0,0 +1,54 @@
+# Example 1, do not modify!
+print(8 / 2)
+
+# Example 2, do not modify!
+print(2**2)
+# Addition, no print output
+print(10 + 2)
+
+# Multiplication, with print output
+print(10 * 2)
+# Create the variable total and print the total revenue
+revenue_1=500
+revenue_2=450
+revenue_3=625
+total = revenue_1 + revenue_2 + revenue_3
+print(total)
+# Print the total revenue
+total = revenue_1 + revenue_2 + revenue_3
+print(total)
+
+# Print the average revenue
+average = (revenue_1 + revenue_2 + revenue_3) / 3
+print(average)
+# Create company_1
+company_1 = str('Apple')
+print(company_1)
+# Create year_1
+year_1 = int(2017)
+print(year_1)
+# Create revenue_1
+revenue_1 = float(229.23)
+print(revenue_1)
+# Type of company_1
+print(type(company_1))
+
+# Type of year_1
+print(type(year_1))
+
+# Type of revenue_1
+print(type(revenue_1))
+# Test equality
+test_company = 'apple'
+print(company_1 == test_company)
+# Compare revenue_1 and revenue_2
+print(revenue_1 > revenue_2)
+# Update data types
+year_1_str = str(year_1)
+(revenue_1_str) = str(revenue_1)
+
+# Create a complete sentence combining only the string data types
+sentence = 'The revenue of ' + company_1 + ' in ' + year_1_str + ' was $' + revenue_1_str + ' billion.'
+
+# Print sentence
+print(sentence)
